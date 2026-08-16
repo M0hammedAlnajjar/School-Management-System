761a1f4fd6a7529a487d593ecabfc778ea33d33c
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-16 23:34:36 +0400

    faat:I generate the getters and setters

--- src/entities/Teacher.java
+++ src/entities/Teacher.java
@@ -8,4 +8,35 @@ public class Teacher {
     private double salary;
     private List<String> classesTaught;
 
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public List<String> getClassesTaught() {
+        return classesTaught;
+    }
+
+    public void setClassesTaught(List<String> classesTaught) {
+        this.classesTaught = classesTaught;
+    }
 }
