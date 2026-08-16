2559e7106d4198710f6d3fd28ea444aa6db42499
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-16 19:57:04 +0400

    faat:I careate private fields

---
+++ src/entities/Person.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Person {
+private String id;
+private String firstName;
+private String lastName;
+private String dateOfBirth;
+private String gender;
+private String phoneNumber;
+private String email;
+private String address;
+}
