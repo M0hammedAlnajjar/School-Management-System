1d41c5f2981cf6d5479e8654f4f5639973967f0d
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-16 20:50:55 +0400

    faat:I careate getters and setters

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -7,4 +7,36 @@ public class Student {
     private String enrollmentDate;
     private double feeBalance;
     private List<String> subjects;
+
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(String gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+
+    public String getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(String enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public double getFeeBalance() {
+        return feeBalance;
+    }
+
+    public void setFeeBalance(double feeBalance) {
+        this.feeBalance = feeBalance;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
 }
