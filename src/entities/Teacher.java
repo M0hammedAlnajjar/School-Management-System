dbd1d0a3498cc04fdcd64b43269e83bc81c5c175
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-16 23:31:15 +0400

    feat: add Teacher entity and private fields

---
+++ src/entities/Teacher.java
@@ -0,0 +1,11 @@
+package entities;
+
+import java.util.List;
+
+public class Teacher {
+    private String subject;
+    private int experienceYears;
+    private double salary;
+    private List<String> classesTaught;
+
+}
