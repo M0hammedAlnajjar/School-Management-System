5ae084c43ec22513dfa36156800053e669561769
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-17 18:45:12 +0400

    feat(HeadTeacher): update constructor and management logic

--- src/entities/HeadTeacher.java
+++ src/entities/HeadTeacher.java
@@ -3,66 +3,41 @@ package entities;
 import java.util.List;
 
 public class HeadTeacher extends Teacher {
-    private double budgetAllocated;
-    private String officeNumber;
-    private int yearsOfExperience;
-    private String email;
-    private boolean isFullTime;
+    private String department;
+    private int teachersManaged;
 
-
-    public HeadTeacher(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, int experienceYears, String subject, double salary, List<String> classesTaught, double budgetAllocated, String officeNumber, int yearsOfExperience, String email1, boolean isFullTime) {
+    public HeadTeacher(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, int experienceYears, String subject, double salary, List<String> classesTaught) {
         super(firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, experienceYears, subject, salary, classesTaught);
-        this.budgetAllocated = budgetAllocated;
-        this.officeNumber = officeNumber;
-        this.yearsOfExperience = yearsOfExperience;
-        this.email = email1;
-        this.isFullTime = isFullTime;
     }
 
-    public double getBudgetAllocated() {
-        return budgetAllocated;
+
+    public String getDepartment() {
+        return department;
     }
 
-    public void setBudgetAllocated(double budgetAllocated) {
-        this.budgetAllocated = budgetAllocated;
+    public void setDepartment(String department) {
+        this.department = department;
     }
 
-    public String getOfficeNumber() {
-        return officeNumber;
+    public int getTeachersManaged() {
+        return teachersManaged;
     }
 
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
+    public void setTeachersManaged(int teachersManaged) {
+        this.teachersManaged = teachersManaged;
     }
 
-    public int getYearsOfExperience() {
-        return yearsOfExperience;
+
+    public void addManagedTeacher() {
+        teachersManaged = teachersManaged + 1;
     }
 
-    public void setYearsOfExperience(int yearsOfExperience) {
-        this.yearsOfExperience = yearsOfExperience;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isFullTime() {
-        return isFullTime;
-    }
-
-    public void setFullTime(boolean fullTime) {
-        isFullTime = fullTime;
-    }
 
     @Override
     public void displayInfo() {
         super.displayInfo();
-        System.out.println("Head Teacher: " + getFirstName() + " " + getLastName()
-                + " | budgetAllocated: " + budgetAllocated+ " | officeNumber" +officeNumber + " | yearsOfExperience: " + yearsOfExperience+ " | email: " + email+ "isFullTime"+isFullTime);
-    }
-}
+         {
+            System.out.println("Head Teacher: " + getFirstName() + " " + getLastName()
+                    + " | department: " + department + " | manages: " + teachersManaged + " teachers");
+        }
+}}
