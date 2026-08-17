e68a6a451efb5b3775817fabb9e927e0bf5ca43a
mohammed <mooodo150@gmail.com>
2026-08-17 09:27:53 +0400

    feat:I add in entites class named is Senior student and i generate getter and setters

---
+++ src/entities/SeniorStudent.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class SeniorStudent {
+    private String major;
+    private double gpa;
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
