e4fe0aee6be9e68df9e4a154ad884ff186f5516b
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-17 20:17:17 +0400

    feat(SchoolApp): introduce console menu layout

--- src/SchoolApp.java
+++ src/SchoolApp.java
@@ -14,6 +14,39 @@ public class SchoolApp {
     }
 
     private void run() {
+        boolean exit = false;
+        while (exit==false) {
+            printMenu();
+            int choice = input.getIntInput("Choose:", 1, 4);
+            switch (choice) {
+                case 1: addStudent(); break;
+                case 2: viewAllStudents(); break;
+                case 3: searchStudent(); break;
+                case 4: exit = true; break;
+            }
+        
+        System.out.println("Goodbye.");
+    }
+
+        
+    }
+
+    private void addStudent() {
+    }
+
+    private void viewAllStudents() {
+    }
+
+    private void searchStudent() {
+    }
+
+    private void printMenu() {
+        System.out.println();
+        System.out.println("=== School Menu ===");
+        System.out.println("1. Add student");
+        System.out.println("2. View all students");
+        System.out.println("3. Search student by id");
+        System.out.println("4. Exit");
     }
 
     private void loadSampleData() {
--- src/utils/InputHandler.java
+++ src/utils/InputHandler.java
@@ -3,4 +3,9 @@ package utils;
 public class InputHandler {
     private String department;
     private int teachersManaged;
+
+
+    public int getIntInput(String s, int i, int i1) {
+        return 0;
+    }
 }
