5cc1f9fa81dae79dcaeb711a922525c90d3ac91e
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-17 19:42:43 +0400

    feat(app): add SchoolApp entry point with StudentService and InputHandler

--- src/SchoolApp.java
+++ src/SchoolApp.java
@@ -1,4 +1,28 @@
-void main() {
+import services.StudentService;
+import utils.InputHandler;
+
+public class SchoolApp {
 
 
+    private StudentService studentService = new StudentService();
+    private InputHandler input = new InputHandler();
+
+    public static void main(String[] args) {
+    }
+
+    public InputHandler getInput() {
+        return input;
+    }
+
+    public void setInput(InputHandler input) {
+        this.input = input;
+    }
+
+    public StudentService getStudentService() {
+        return studentService;
+    }
+
+    public void setStudentService(StudentService studentService) {
+        this.studentService = studentService;
+    }
 }
