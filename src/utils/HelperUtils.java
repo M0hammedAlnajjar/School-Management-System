04a28fd6397a8548232a42c977ed45fd4e198a62
mohammed <mooodo150@gmail.com>
2026-08-17 11:12:58 +0400

    feat(utils): extend HelperUtils from Teacher and add constructor

--- src/utils/HelperUtils.java
+++ src/utils/HelperUtils.java
@@ -1,8 +1,16 @@
 package utils;
 
-public class HelperUtils  {
+import entities.Teacher;
+
+import java.util.List;
+
+public class HelperUtils extends Teacher {
     private String major;
     private double GPA;
+
+    public HelperUtils(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, int experienceYears, String subject, double salary, List<String> classesTaught) {
+        super(firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, experienceYears, subject, salary, classesTaught);
+    }
 
     public String getMajor() {
         return major;
