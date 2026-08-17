8638cf51e87760b1deb4b6484d7227ee8bffb0e3
mohammed <mooodo150@gmail.com>
2026-08-17 09:30:07 +0400

    feat:I useing extends and generate constructor

--- src/entities/SeniorStudent.java
+++ src/entities/SeniorStudent.java
@@ -1,8 +1,14 @@
 package entities;
 
-public class SeniorStudent {
+import java.util.List;
+
+public class SeniorStudent extends Student {
     private String major;
     private double gpa;
+
+    public SeniorStudent(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String gradeLevel, double feeBalance, List<String> subjects, String enrollmentDate) {
+        super(firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, gradeLevel, feeBalance, subjects, enrollmentDate);
+    }
 
     public String getMajor() {
         return major;
