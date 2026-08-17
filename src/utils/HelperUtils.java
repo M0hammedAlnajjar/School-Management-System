8de7215efc040ac161ca022042558c571f1d37fc
mohammed <mooodo150@gmail.com>
2026-08-17 11:19:29 +0400

    fix(utils): Modify class HelperUtils

--- src/utils/HelperUtils.java
+++ src/utils/HelperUtils.java
@@ -1,30 +1,14 @@
 package utils;
 
-import entities.Teacher;
+public class HelperUtils {
 
-import java.util.List;
+    private static int counter = 1;
 
-public class HelperUtils extends Teacher {
-    private String major;
-    private double GPA;
-
-    public HelperUtils(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, int experienceYears, String subject, double salary, List<String> classesTaught) {
-        super(firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, experienceYears, subject, salary, classesTaught);
+    public static int getCounter() {
+        return counter;
     }
 
-    public String getMajor() {
-        return major;
-    }
-
-    public void setMajor(String major) {
-        this.major = major;
-    }
-
-    public double getGPA() {
-        return GPA;
-    }
-
-    public void setGPA(double GPA) {
-        this.GPA = GPA;
+    public static void setCounter(int counter) {
+        HelperUtils.counter = counter;
     }
 }
