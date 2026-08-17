7d30fed09f801ce01fbaf56488b6960aeff164e5
mohammed <mooodo150@gmail.com>
2026-08-17 11:09:22 +0400

    feat(utils): add HelperUtils with major and GPA fields

--- src/utils/HelperUtils.java
+++ src/utils/HelperUtils.java
@@ -1,4 +1,22 @@
 package utils;
 
-public class HelperUtils {
+public class HelperUtils  {
+    private String major;
+    private double GPA;
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
 }
