a57884445b283b4645b7194939ac69061f8470c5
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-18 19:15:49 +0400

    fix: prevent null pointer in person equality check

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,8 +1,10 @@
 package entities;
 
 import interfaces.Displayable;
+import utils.HelperUtils;
 
 public class Person implements Displayable {
+
     private String id;
     private String firstName;
     private String lastName;
@@ -12,123 +14,211 @@ public class Person implements Displayable {
     private String email;
     private String address;
 
-
+    // Empty Constructor
     public Person() {
 
     }
 
+    // Constructor
+    public Person(
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String gender,
+            String phoneNumber,
+            String email,
+            String address
+    ) {
 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    // ID Getter
     public String getId() {
+
         return id;
     }
 
+    // ID Setter
     public void setId(String id) {
-        if (utils.HelperUtils.isEmpty(id)) {
-            System.out.println("Id cannot be empty. Keeping the old id.");
+
+        if (HelperUtils.isEmpty(id)) {
+
+            System.out.println(
+                    "ID cannot be empty. Keeping the old ID."
+            );
+
             return;
         }
+
         this.id = id;
     }
 
+    // First Name Getter
     public String getFirstName() {
+
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    // First Name Setter
+    public void setFirstName(
+            String firstName
+    ) {
+
         this.firstName = firstName;
     }
 
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
+    // Last Name Getter
     public String getLastName() {
+
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    // Last Name Setter
+    public void setLastName(
+            String lastName
+    ) {
+
         this.lastName = lastName;
     }
 
-    public Person(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    // Date of Birth Getter
+    public String getDateOfBirth() {
+
+        return dateOfBirth;
+    }
+
+    // Date of Birth Setter
+    public void setDateOfBirth(
+            String dateOfBirth
+    ) {
+
         this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
-
     }
 
+    // Gender Getter
+    public String getGender() {
 
+        return gender;
+    }
+
+    // Gender Setter
+    public void setGender(String gender) {
+
+        this.gender = gender;
+    }
+
+    // Phone Number Getter
+    public String getPhoneNumber() {
+
+        return phoneNumber;
+    }
+
+    // Phone Number Setter
+    public void setPhoneNumber(
+            String phoneNumber
+    ) {
+
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Email Getter
+    public String getEmail() {
+
+        return email;
+    }
+
+    // Email Setter
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+
+    // Address Getter
+    public String getAddress() {
+
+        return address;
+    }
+
+    // Address Setter
+    public void setAddress(
+            String address
+    ) {
+
+        this.address = address;
+    }
+
+    // Display Full Information
     @Override
     public void displayInfo() {
-        System.out.println("Person: " + firstName + " " + lastName + " (id: " + id + ")");
+
+        System.out.println(
+                "Person: "
+                        + firstName
+                        + " "
+                        + lastName
+                        + " (ID: "
+                        + id
+                        + ")"
+        );
     }
 
+    // Display Short Information
     @Override
     public String displaySummary() {
-        return id + " - " + firstName + " " + lastName;
+
+        return id
+                + " - "
+                + firstName
+                + " "
+                + lastName;
     }
 
-
+    // Convert Object to String
     @Override
     public String toString() {
 
-        return "Person{id=" + id + ", name=" + firstName + " " + lastName + "}";
+        return "Person{id="
+                + id
+                + ", name="
+                + firstName
+                + " "
+                + lastName
+                + "}";
     }
 
+    // Compare Two Person Objects
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
+    public boolean equals(Object object) {
+
+        if (this == object) {
+
             return true;
+        }
 
-        if (obj == null)
+        if (object == null) {
+
             return false;
+        }
 
+        if (!(object instanceof Person)) {
 
-        if (!(obj instanceof Person)) return false;
-        Person p = (Person) obj;
-        return this.id.equals(p.id);
+            return false;
+        }
 
+        Person person = (Person) object;
+
+        if (this.id == null) {
+
+            return person.id == null;
+        }
+
+        return this.id.equals(person.id);
     }
-
-
 }
