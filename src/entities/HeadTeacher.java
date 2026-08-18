12efdf8e8170e74b54ae9868de6282599935b0f2
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-18 19:22:05 +0400

    fix(Head Teacher): initialize head teacher department and managed teachers

--- src/entities/HeadTeacher.java
+++ src/entities/HeadTeacher.java
@@ -3,42 +3,97 @@ package entities;
 import java.util.List;
 
 public class HeadTeacher extends Teacher {
+
     private String department;
     private int teachersManaged;
 
-    public HeadTeacher(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, int experienceYears, String subject, double salary, List<String> classesTaught) {
-        super(firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, experienceYears, subject, salary, classesTaught);
+    // Constructor
+    public HeadTeacher(
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String gender,
+            String phoneNumber,
+            String email,
+            String address,
+            int experienceYears,
+            String subject,
+            double salary,
+            List<String> classesTaught,
+            String department,
+            int teachersManaged
+    ) {
+
+        super(
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                experienceYears,
+                subject,
+                salary,
+                classesTaught
+        );
+
         this.department = department;
-    }
-
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public int getTeachersManaged() {
-        return teachersManaged;
-    }
-
-    public void setTeachersManaged(int teachersManaged) {
         this.teachersManaged = teachersManaged;
     }
 
+    // Department Getter
+    public String getDepartment() {
 
-    public void addManagedTeacher() {
-        teachersManaged = teachersManaged + 1;
+        return department;
     }
 
+    // Department Setter
+    public void setDepartment(
+            String department
+    ) {
 
+        this.department = department;
+    }
+
+    // Teachers Managed Getter
+    public int getTeachersManaged() {
+
+        return teachersManaged;
+    }
+
+    // Teachers Managed Setter
+    public void setTeachersManaged(
+            int teachersManaged
+    ) {
+
+        this.teachersManaged =
+                teachersManaged;
+    }
+
+    // Increase Number of Managed Teachers
+    public void addManagedTeacher() {
+
+        teachersManaged =
+                teachersManaged + 1;
+    }
+
+    // Display Head Teacher Information
     @Override
     public void displayInfo() {
+
         super.displayInfo();
-         {
-            System.out.println("Head Teacher: " + getFirstName() + " " + getLastName()
-                    + " | department: " + department + " | manages: " + teachersManaged + " teachers");
-        }
-}}
+
+        System.out.println(
+                "Head Teacher: "
+                        + getFirstName()
+                        + " "
+                        + getLastName()
+                        + " | Department: "
+                        + department
+                        + " | Manages: "
+                        + teachersManaged
+                        + " teachers"
+        );
+    }
+}
