d8f6659b9c3cd4e84877aaf8ab50f9012433535e
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-18 19:20:55 +0400

    fix(entites): display inherited senior student information

--- src/entities/SeniorStudent.java
+++ src/entities/SeniorStudent.java
@@ -3,37 +3,85 @@ package entities;
 import java.util.List;
 
 public class SeniorStudent extends Student {
+
     private String major;
     private double gpa;
 
+    // Constructor
+    public SeniorStudent(
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String gender,
+            String phoneNumber,
+            String email,
+            String address,
+            String gradeLevel,
+            double feeBalance,
+            List<String> subjects,
+            String enrollmentDate,
+            String major,
+            double gpa
+    ) {
 
-    public SeniorStudent(String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String gradeLevel, double feeBalance, List<String> subjects, String enrollmentDate, String major, double gpa) {
-        super(firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, gradeLevel, feeBalance, subjects, enrollmentDate);
+        super(
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                gradeLevel,
+                feeBalance,
+                subjects,
+                enrollmentDate
+        );
+
         this.major = major;
         this.gpa = gpa;
     }
 
+    // Major Getter
     public String getMajor() {
+
         return major;
     }
 
-    public void setMajor(String major) {
+    // Major Setter
+    public void setMajor(
+            String major
+    ) {
+
         this.major = major;
     }
 
+    // GPA Getter
     public double getGpa() {
+
         return gpa;
     }
 
-    public void setGpa(double gpa) {
+    // GPA Setter
+    public void setGpa(
+            double gpa
+    ) {
+
         this.gpa = gpa;
     }
 
+    // Display Senior Student Information
     @Override
     public void displayInfo() {
-        System.out.println(" SeniorStudent: " + getFirstName());
-        System.out.println("Major: " + getMajor());
-        System.out.println("GPA: " + getGpa());
 
+        super.displayInfo();
+
+        System.out.println(
+                "Major: " + major
+        );
+
+        System.out.println(
+                "GPA: " + gpa
+        );
     }
 }
